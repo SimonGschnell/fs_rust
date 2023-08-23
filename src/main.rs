62f7a0df5be7ac2d6b248de1209bc5dc690aa5bc
Simon Gschnell <simongschnell@gmail.com>
2023-08-23 14:06:36 +0200

    OsString usage, default path added

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
-use std::{path::{Path} , fs::{read_dir}};
-
+use std::{path::{Path, PathBuf} , fs::{read_dir}, slice::SliceIndex};
+use std::env::{args_os,ArgsOs};
+use std::ffi::OsString;
 
 fn main() {
- recursive_dotfiles("./root")   
+    let mut args = args_os();
+    
+ recursive_dotfiles(args.nth(1).unwrap_or(OsString::from("./example"))); 
 }
 
-fn recursive_dotfiles(path:&str){
+fn recursive_dotfiles(path:OsString){
 
-    let root = read_dir(Path::new(path)).unwrap();
+    let root = read_dir(PathBuf::from(path).as_path()).unwrap();
     for entry in root{
         if let Ok(e) = entry{
             let filename= e.file_name();
+            let file_str = filename.to_str().unwrap_or("placeholder");
             
-            if "." == &filename.to_str().unwrap()[0..1]{
-                println!("{:?}",e.path());
-            }
+                if let Some(first_letter)=file_str.get(0..1){
+                    if "." == first_letter{
+                        println!("{:?}",e.path());
+                    }
+                }
+                
+           
+            
 
             if let Ok(data) = e.metadata(){
                 if data.is_dir(){
-                    recursive_dotfiles(e.path().to_str().unwrap())
+                    recursive_dotfiles(e.path().into_os_string())
                 }
             }
         }
